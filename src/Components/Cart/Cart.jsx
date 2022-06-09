1d5631730336572a1b5a3c7e3eb17ba904aea0fe
Ilika Ysakova <ilikaysakova@MacBook-Air-Ilika.local>
2022-06-09 19:32:16 +0600

    comm

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { cartContext } from "../Context/CartContext";
+import MainNavbar from "../MainNavbar/MainNavbar";
 
 const Cart = () => {
   const { cart, getCart, deleteCartService, changeServiceCount } =
@@ -9,50 +10,53 @@ const Cart = () => {
     getCart();
   }, []);
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Фото</th>
-            <th>Позиция</th>
-            <th>Цена</th>
-            <th>Количество</th>
-            <th>Итого</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.service
-            ? cart.service.map((elem) => (
-                <tr key={elem.item.id}>
-                  <td>
-                    <img width={50} src={elem.item.img1} alt="phone" />
-                  </td>
-                  <td>{elem.item.title}</td>
-                  <td>{elem.item.price}</td>
-                  <td>
-                    <input
-                      type="number"
-                      value={elem.count}
-                      onChange={(e) =>
-                        changeServiceCount(elem.item.id, e.target.value)
-                      }
-                    />
-                  </td>
-                  <td>{elem.subPrice}</td>
-                  <td>
-                    <button onClick={() => deleteCartService(elem.item.id)}>
-                      Удалить
-                    </button>
-                  </td>
-                </tr>
-              ))
-            : null}
-        </tbody>
-      </table>
+    <>
+      <MainNavbar />
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Фото</th>
+              <th>Позиция</th>
+              <th>Цена</th>
+              <th>Количество</th>
+              <th>Итого</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.service
+              ? cart.service.map((elem) => (
+                  <tr key={elem.item.id}>
+                    <td>
+                      <img width={50} src={elem.item.img} alt="phone" />
+                    </td>
+                    <td>{elem.item.title}</td>
+                    <td>{elem.item.price}</td>
+                    <td>
+                      <input
+                        type="number"
+                        value={elem.count}
+                        onChange={(e) =>
+                          changeServiceCount(elem.item.id, e.target.value)
+                        }
+                      />
+                    </td>
+                    <td>{elem.subPrice}</td>
+                    <td>
+                      <button onClick={() => deleteCartService(elem.item.id)}>
+                        Удалить
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              : null}
+          </tbody>
+        </table>
 
-      <h4>Общая сумма: {cart.totalPrice}</h4>
-      <button>Оформить</button>
-    </div>
+        <h4>Общая сумма: {cart.totalPrice}</h4>
+        <button>Оформить</button>
+      </div>
+    </>
   );
 };
 
