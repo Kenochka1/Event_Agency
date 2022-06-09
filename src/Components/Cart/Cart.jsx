be31c1f82eb3d01afbfee5bc63451626f446dfeb
Ilika Ysakova <ilikaysakova@MacBook-Air-Ilika.local>
2022-06-09 21:31:59 +0600

    gogoogogogogog

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -13,7 +13,6 @@ const Cart = () => {
   }, []);
   return (
     <>
-<<<<<<< HEAD
       <MainNavbar />
       <div>
         <table>
@@ -56,82 +55,6 @@ const Cart = () => {
           </tbody>
         </table>
 
-=======
-      <MainNavbar />{" "}
-      <div className="mainContainer-cart">
-        <div className="container-cart">
-          <table>
-            <thead>
-              <tr className="cart-mainText">
-                <th data-aos="flip-left" id="mainText">
-                  Фото
-                </th>
-                <th data-aos="flip-left" id="mainText">
-                  Название
-                </th>
-                <th data-aos="flip-left" id="mainText">
-                  Цена
-                </th>
-                <th data-aos="flip-left" id="mainText">
-                  Кол
-                </th>
-                <th data-aos="flip-left" id="mainText">
-                  Итого
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {cart.service
-                ? cart.service.map((elem) => (
-                    <tr key={elem.item.id}>
-                      <td>
-                        <img
-                          data-aos="fade-right"
-                          className="elem-img"
-                          width={200}
-                          src={elem.item.img}
-                          alt="phone"
-                        />
-                      </td>
-                      <td data-aos="flip-left" className="elem-title">
-                        {elem.item.title}
-                      </td>
-                      <td data-aos="flip-left" className="elem-price">
-                        {elem.item.price}
-                      </td>
-                      <td>
-                        <input
-                          data-aos="flip-left"
-                          id="input-cart"
-                          type="number"
-                          style={{ width: "70px" }}
-                          value={elem.count}
-                          onChange={(e) =>
-                            changeServiceCount(elem.item.id, e.target.value)
-                          }
-                        />
-                      </td>
-                      <td data-aos="flip-left" className="elem-subPrice">
-                        {elem.subPrice}
-                      </td>
-                      <td>
-                        <button
-                          data-aos="fade-left"
-                          id="delete"
-                          onClick={() => deleteCartService(elem.item.id)}
-                        >
-                          Удалить
-                        </button>
-                      </td>
-                    </tr>
-                  ))
-                : null}
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div className="container-pay2">
->>>>>>> 26f7d548523e2b6ef1d5be1bac39414b34f4ddaa
         <h4>Общая сумма: {cart.totalPrice}</h4>
       </div>
       <div className="container-pay">
